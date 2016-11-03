3dc76598d617f8224c66bb8dbf0b0c39f3b710b1
shutokondo <tech-camp@tech-camp-2-007-no-MacBook-Air.local>
2016-11-04 01:08:21 +0900

    modify users columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('family_name');
+            $table->string('first_name');
+            $table->integer('age');
+            $table->string('prefecture');
             $table->timestamps();
         });
     }
