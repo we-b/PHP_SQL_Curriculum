5b10ca7aa3c98f188b24ce3da846cd314ea231ac
shutokondo <tech-camp@tech-camp-2-007-no-MacBook-Air.local>
2016-11-04 01:09:11 +0900

    set database seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        Model::unguard();
+
+        $this->call(UsersTableSeeder::class);
+        $this->call(ShiftsTableSeeder::class);
     }
 }
---
+++ database/seeds/ShiftsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Goodby\CSV\Import\Standard\Lexer;
+use Goodby\CSV\Import\Standard\Interpreter;
+use Goodby\CSV\Import\Standard\LexerConfig;
+use App\Shift;
+
+class ShiftsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $config = new LexerConfig();
+        $interpreter = new Interpreter();
+        $interpreter->addObserver(function(array $columns) {
+            Shift::create(array(
+                "date"        => $columns[1],
+                "start_time"  => $columns[2],
+                "user_id"     => $columns[3],
+            ));
+        });
+
+        $lexer = new Lexer($config);
+        $lexer->parse(database_path() . '/seeds/csv/shifts.csv', $interpreter);
+    }
+}
